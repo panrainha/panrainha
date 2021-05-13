4f8b8ccafef9178549fbc5bb42f1734c48662624
Eliabe Silva <eliabe.silva.la@gmail.com>
2021-05-13 19:46:19 -0300

    update pages/landing.js

--- pages/landing.js
+++ pages/landing.js
@@ -43,7 +43,7 @@ export default function LandingPage(props) {
         }}
         {...rest}
       />
-      <Parallax filter responsive image={require("assets/img/DSC00500.jpg")}>
+      <Parallax filter responsive image={require("assets/img/fundo.jpg")}>
         <div className={classes.container}>
           <GridContainer>
             <GridItem xs={12} sm={12} md={6}>
